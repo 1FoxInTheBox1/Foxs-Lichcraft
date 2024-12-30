c2fcdbc876c833c1726bec727621a397fb5884f8
1FoxInTheBox1 <44453935+1FoxInTheBox1@users.noreply.github.com>
2024-12-30 16:46:48 -0600

    Initial commit

---
+++ src/main/java/com/foxinthebox/lichcraft/FoxsLichcraftClient.java
@@ -0,0 +1,10 @@
+package com.foxinthebox.lichcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoxsLichcraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
