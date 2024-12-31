0b96923c17b42d086b1f9e92c50708b0dc9e769e
1FoxInTheBox1 <44453935+1FoxInTheBox1@users.noreply.github.com>
2024-12-31 14:11:47 -0600

    Item Group

--- src/main/java/com/foxinthebox/lichcraft/item/ModItems.java
+++ src/main/java/com/foxinthebox/lichcraft/item/ModItems.java
@@ -1,15 +1,28 @@
 package com.foxinthebox.lichcraft.item;
 
 import com.foxinthebox.lichcraft.FoxsLichcraft;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    // Items
     public static final Item SOUL_GOO = register(new Item.Settings(), "soul_goo");
+
+    // Item Group
+    public static final RegistryKey<ItemGroup> ITEM_GROUP_KEY = RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of(FoxsLichcraft.MOD_ID, "item_group"));
+    public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.SOUL_GOO))
+            .displayName(Text.translatable("itemGroup.lichcraft"))
+            .build();
 
     public static Item register(Item.Settings itemSettings, String id) {
         Identifier itemID = Identifier.of(FoxsLichcraft.MOD_ID, id);
@@ -20,5 +33,10 @@ public class ModItems {
     }
 
     public static void initialize() {
+        Registry.register(Registries.ITEM_GROUP, ITEM_GROUP_KEY, ITEM_GROUP);
+
+        ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP_KEY).register(itemGroup -> {
+            itemGroup.add(ModItems.SOUL_GOO);
+        });
     }
 }
